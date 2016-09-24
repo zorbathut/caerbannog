2f2d1b08d51a5dc079fb9fa244a5bfed3a80865c
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2016-09-24 07:25:39 -0700

    Initial checkin; seems to work!

---
+++ IncidentWorker_ManhunterPack_Caerbannog.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Caerbannog
+{
+    public class IncidentWorker_ManhunterPack_Caerbannog : IncidentWorker_ManhunterPack
+    {
+        public override bool TryExecute(IncidentParms parms)
+        {
+            def.pawnKinds = (from k in DefDatabase<PawnKindDef>.AllDefs
+                             where k.RaceProps.Animal
+                             select k).ToList();
+
+            return base.TryExecute(parms);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# The Mad Rabbits of Caerbannog
+Rimworld The Mad Rabbits of Caerbannog mod
+
+By default, the Manhunter Pack event picks from a very limited set of animals.
+
+That means you can't be invaded by bloodthirsty packs of furious rabbits.
+
+I consider this an unfortunate oversight, and have fixed it.
