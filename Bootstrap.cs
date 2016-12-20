24ecab4a1b7abf7714ef3a904ed5fe988c1b3ef8
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2016-12-20 10:06:35 -0800

    Rewrite Caerbannog for A16.

---
+++ Bootstrap.cs
@@ -0,0 +1,26 @@
+﻿using CommunityCoreLibrary_Caerbannog;
+using System;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace Caerbannog
+{
+    class Bootstrap : Def
+    {
+        public string ModName;
+
+        static Bootstrap()
+        {
+            {
+                MethodInfo method1 = typeof(RimWorld.ManhunterPackIncidentUtility).GetMethod("TryFindRandomAnimalKind", BindingFlags.Static | BindingFlags.Public);
+                MethodInfo method2 = typeof(ManhunterPackIncidentUtility_Detour).GetMethod("TryFindRandomAnimalKind", BindingFlags.Static | BindingFlags.Public);
+                if (!Detours.TryDetourFromTo(method1, method2))
+                {
+                    Log.Error("EVERYTHING IS BROKEN 1");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ ManhunterPackIncidentUtility_Detour.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Caerbannog
+{
+    static class ManhunterPackIncidentUtility_Detour
+    {
+        public static bool TryFindRandomAnimalKind(List<PawnKindDef> candidates, Map map, out PawnKindDef animalKind)
+        {
+            return (from k in DefDatabase<PawnKindDef>.AllDefs
+                    where k.RaceProps.Animal && (map == null || map.mapTemperature.SeasonAndOutdoorTemperatureAcceptableFor(k.race))
+                    select k).TryRandomElement(out animalKind);
+        }
+    }
+}
